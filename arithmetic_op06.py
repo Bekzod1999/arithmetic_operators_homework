5a5111104cb789c599f3e16a327389c45618716e
businessman_1999 <developmentfirm1999@gmail.com>
2022-08-09 18:32:39 +0500

    06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,5 @@
 #Create a variable called 'number' with data type int.
-number = 11553
+number = 12
 #Divide the number by 2 and assign number.
 number = number / 2 
 #Find the fraction of the number and assign to a variable called 'answer'.
