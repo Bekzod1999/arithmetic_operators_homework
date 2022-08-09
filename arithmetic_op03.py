c02fe2b80e43e6820445c3a4a3d0aa5816fe189e
businessman_1999 <developmentfirm1999@gmail.com>
2022-08-09 18:17:28 +0500

    op03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,13 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 98
+a = number % 10 
+a = str(a)
+number //= 10 
+number = str(number)
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = a + number
 #Print the answer variable
+print(answer)
+
+#I solved on this task depending on the topic you taught us
+#So, I converted value int to string
