08fda8f58a517a1dc88957e6612e07d6de7c8424
businessman_1999 <developmentfirm1999@gmail.com>
2022-08-09 18:20:48 +0500

    op06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,11 @@
 #Create a variable called 'number' with data type int.
- 
+number = 11553
 #Divide the number by 2 and assign number.
- 
-#Find the fraction of the number and assing to a variable called 'answer'.
- 
+number = number / 2 
+#Find the fraction of the number and assign to a variable called 'answer'.
+a = number * 10
+answer = int(a % 10)
 #Print the answer.
- 
+print(answer) 
 #Print the number.
+print(number)
